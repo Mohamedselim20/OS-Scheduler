ab471835ab703ed1a8606aac36ec294730a60e73
Mohamedselim20 <166921412+Mohamedselim20@users.noreply.github.com>
2025-08-06 18:04:55 +0300

    Update process.c

--- process.c
+++ process.c
@@ -1,27 +1,54 @@
-#include "headers.h"
 
-// Handle termination
+#include "headers.h"
 void handl(int signum)
 {
     destroyClk(false);
-    raise(SIGKILL);  // terminate the process
+    raise(SIGKILL);
 }
-
-int main(int argc, char * argv[])
+/* Modify this file as needed*/
+int main(int agrc, char * argv[])
 {
-    signal(SIGCHLD, handl);
     initClk();
-
-    // ========== Parse shared memory ID from arguments ==========
-    if (argc < 2)
+    //TODO it needs to get the remaining time from somewhere
+    //remainingtime = ??;
+    signal(SIGCHLD,handl);
+    key_t shmKey1,prevClkKey;
+    shmKey1=ftok("keyfile",65);
+    prevClkKey=ftok("keyfile",100);
+    int prevClkID=shmget(prevClkKey,sizeof(int),0666);
+    if (prevClkID == -1)
     {
-        fprintf(stderr, "Error: Missing shared memory ID argument.\n");
-        exit(EXIT_FAILURE);
+        perror("Error in getting the shared memory");
+        exit(-1);
     }
-
-    int shmid = atoi(argv[1]);
-    int *remainingTime = (int *) shmat(shmid, NULL, 0);
-    if (remainingTime == (void *) -1)
+    int shmid1=shmget(shmKey1,sizeof(int),0666);
+    if (shmid1 == -1)
     {
-        perror("Error attaching to shared memory");
-        exit(EXIT_FAILURE);
+        perror("Error in getting the shared memory");
+        exit(-1);
+    }
+    int *shmaddr =(int *) shmat(shmid1, (void *)0, 0);
+    int *prev=(int *) shmat(prevClkID, (void *)0, 0);
+    *prev=getClk();
+    while ((*shmaddr) > 0)
+    {
+       fflush(stdout);
+       while(*prev==getClk())
+       {
+           // Loop until prev == getClk()
+       }
+       while(*prev==getClk())
+       {
+           // Ensure that the condition is true
+       }
+       *prev=getClk();
+       (*shmaddr)--;
+       // printf("remaining time is : %d, pid: %d \n",*shmaddr,getpid());
+       fflush(stdout);
+     
+    }
+    shmdt(shmaddr);
+    shmdt(prev);
+    destroyClk(false);
+    return 0;
+}
