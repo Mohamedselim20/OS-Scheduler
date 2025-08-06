a093c3fb74b4fc184c0f71ccecae73527f236582
Mohamedselim20 <166921412+Mohamedselim20@users.noreply.github.com>
2025-08-06 19:25:44 +0300

    Update process.c

--- process.c
+++ process.c
@@ -1,53 +1,49 @@
 #include "headers.h"
+
 void handl(int signum)
 {
     destroyClk(false);
-    raise(SIGKILL);
+    raise(SIGKILL);  // kill self
 }
-/* Modify this file as needed*/
-int main(int agrc, char * argv[])
-{
-    initClk();
-    signal(SIGCHLD,handl);
-    key_t shmKey1,prevClkKey;
-    shmKey1=ftok("keyfile",65);
-    prevClkKey=ftok("keyfile",100);
-    int prevClkID=shmget(prevClkKey,sizeof(int),0666);
-    if (prevClkID == -1)
-    {
-        perror("Error in getting the shared memory");
-        exit(-1);
-    }
-    //remaining time shared memory
-    int shmid1=shmget(shmKey1,sizeof(int),0666);
-    if (shmid1 == -1)
-    {
-        perror("Error in getting the shared memory");
-        exit(-1);
-    }
-    int *shmaddr =(int *) shmat(shmid1, (void *)0, 0);
-    int *prev=(int *) shmat(prevClkID, (void *)0, 0);
-    *prev=getClk();
 
-    while ((*shmaddr) > 0)
+int main(int argc, char * argv[])
+{
+    signal(SIGCHLD, handl);
+    initClk();
+
+    if (argc < 2)
     {
-       fflush(stdout);
-       while(*prev==getClk())
-       {
-           //to wait one clock cycle
-       }
-       while(*prev==getClk())
-       {
-           //to wait one cycle after continue running
-       }
-       *prev=getClk();
-       (*shmaddr)--;
-       kill(SIGUSR1, getppid());
-       printf("Remaining time: %d pid: %d\n", *shmaddr, getpid());
-     
+        fprintf(stderr, "Error: Missing shared memory ID.\n");
+        exit(EXIT_FAILURE);
     }
-    shmdt(shmaddr);
-    shmdt(prev);
+
+    int shmid = atoi(argv[1]);
+    int *remainingTime = (int *) shmat(shmid, NULL, 0);
+    if (remainingTime == (void *) -1)
+    {
+        perror("Error attaching shared memory");
+        exit(EXIT_FAILURE);
+    }
+
+    int prevClk = getClk();
+
+    while (*remainingTime > 0)
+    {
+        while (prevClk == getClk()); // wait one tick
+        prevClk = getClk();
+
+        (*remainingTime)--;
+
+        kill(getppid(), SIGUSR1);
+
+        printf("Remaining time: %d, PID: %d\n", *remainingTime, getpid());
+        fflush(stdout);
+    }
+
+    // Cleanup
+    shmdt(remainingTime);
+    shmctl(shmid, IPC_RMID, NULL);
     destroyClk(false);
+
     return 0;
 }
