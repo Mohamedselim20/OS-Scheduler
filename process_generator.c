1de535b25e54a502cdaf5854f0d1c46b1d92d996
Mohamedselim20 <166921412+Mohamedselim20@users.noreply.github.com>
2025-08-06 18:10:41 +0300

    Update process_generator.c

--- process_generator.c
+++ process_generator.c
@@ -1,3 +1,4 @@
+
 #include "headers.h"
 
 void clearResources(int);
@@ -7,8 +8,8 @@ int msgq_id, shmid;
 void arrivalsSharedMemory()
 {
     key_t shmKey;
-    shmKey = ftok("keyfile", 120);
-    shmid = shmget(shmKey, sizeof(int) * 1000, IPC_CREAT | 0666);
+    shmKey=ftok("keyfile",120);
+    shmid=shmget(shmKey, sizeof(int) *1000, IPC_CREAT|0666);
     if (shmid == -1)
     {
         perror("Error in creating the shared memory");
@@ -16,21 +17,21 @@ void arrivalsSharedMemory()
     }
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char * argv[])
 {
     signal(SIGINT, clearResources);
 
-    // Initialization
+    // TODO Initialization
 
     arrivalsSharedMemory();
-    int* arrivals;
-    arrivals = (int*)shmat(shmid, 0, 0);
-    for (int i = 0; i < 1000; i++)
+    int *arrivals;
+    arrivals = (int*) shmat(shmid, 0, 0);
+    for(int i = 0; i < 1000; i++)
         arrivals[i] = 0;
 
     key_t key_id;
     int send_val;
-    FILE* key = fopen("keyfile", "r");
+    FILE *key = fopen("keyfile", "r");
     key_id = ftok("keyfile", 65);
     msgq_id = msgget(key_id, 0666 | IPC_CREAT);
     if (msgq_id == -1)
@@ -43,7 +44,7 @@ int main(int argc, char* argv[])
 
     // ------ 1. Read the input files.
 
-    FILE* file = fopen("processes.txt", "r"); // Opening the File to get the number of processes
+    FILE *file = fopen("processes.txt", "r"); // Opening the File to get the number of processes
     if (file == NULL) {
         printf("Error! File not opened.\n");
         return 1;
@@ -51,8 +52,8 @@ int main(int argc, char* argv[])
     int numberOfProcesses = 0;
     fscanf(file, "%*[^\n]");
     while (!feof(file)) {
-        int i, a, r, p;
-        fscanf(file, "%d\t%d\t%d\t%d", &i, &a, &r, &p);
+        int i, a, r, p, m;
+        fscanf(file, "%d\t%d\t%d\t%d\t%d", &i, &a, &r, &p, &m);
         numberOfProcesses = i;
     }
     printf("Number of Processes: %d\n", numberOfProcesses);
@@ -64,26 +65,27 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    struct processData* processes = malloc(sizeof(struct processData) * numberOfProcesses); // Array of processes
+    struct processData *processes = malloc(sizeof(struct processData) * numberOfProcesses); // Array of processes
     fscanf(file, "%*[^\n]");
     int index = 0;
     printf("***Input***\n");
-    printf("id   arrival   runtime   priority\n");
+    printf("id   arrival   runtime   priority   memsize\n");
     while (index < numberOfProcesses) {
-        int i, a, r, p;
-        fscanf(file, "%d\t%d\t%d\t%d", &i, &a, &r, &p);
+        int i, a, r, p, m;
+        fscanf(file, "%d\t%d\t%d\t%d\t%d", &i, &a, &r, &p, &m);
         processes[index].id = i;
         processes[index].arrivaltime = a;
         processes[index].runningtime = r;
         processes[index].priority = p;
+        processes[index].memsize = m;
         arrivals[a]++;
-        printf("%d\t%d\t%d\t%d\n", i, a, r, p);
+        printf("%d\t%d\t%d\t%d\t%d \n", i, a, r, p, m);
         index++;
     }
     fclose(file); // CLose File
 
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
-
+    
     int algorithm;
     printf("-------------------------------\n");
     printf("Choose a scheduling algorithm:\n");
@@ -104,7 +106,7 @@ int main(int argc, char* argv[])
     // Scheduler
     printf("Forking Scheduler..\n");
     int pid = fork();
-    if (pid == -1)
+    if (pid == -1) 
         perror("Error in creating scheduler process\n");
     else if (pid == 0) {
         system("gcc -Wall -o scheduler.out scheduler.c -lm -fno-stack-protector");
@@ -113,14 +115,14 @@ int main(int argc, char* argv[])
         if (algorithm == 3)
             execl("./scheduler.out", string(numberOfProcesses, n_str), string(algorithm, a_str), string(timeSlot, t_str), NULL);
         else
-            execl("./scheduler.out", string(numberOfProcesses, n_str), string(algorithm, a_str), NULL);
+            execl("./scheduler.out",  string(numberOfProcesses, n_str), string(algorithm, a_str), NULL);
     }
 
     // Clock
     if (pid != 0) {
         printf("Forking Clock..\n");
         int clkpid = fork();
-        if (clkpid == -1)
+        if (clkpid == -1) 
             perror("Error in creating clock process\n");
         else if (clkpid == 0) {
             system("gcc clk.c -o clk.out -fno-stack-protector");
@@ -160,6 +162,6 @@ void clearResources(int signum)
 {
     //TODO Clears all resources in case of interruption
     printf("Clearing\n");
-    msgctl(msgq_id, IPC_RMID, (struct msqid_ds*)0);
+    msgctl(msgq_id, IPC_RMID, (struct msqid_ds *)0);
     raise(SIGKILL);
 }
